304b4a4b23b4c57c7c08f038dcfb923e994175a6
jed1337 <j.caychingco@yahoo.com>
2018-06-10 10:48:34 +0800

    First commit

---
+++ ViewSpotlightPictures.bat
@@ -0,0 +1,37 @@
+@echo off
+
+rem Where the Spotlight images (and other unrelated images) are stored
+cd "%userprofile%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets"
+
+rem Where to extract the Spotlight pictures to
+set tempFolder=tempPictures
+
+rem delete the %tempFolder% if it exists
+rem "/s" = Remove all directories and files in addition to the directory itiself
+rem "/q" = Quiet mode. Don't ask to remove.
+rem It will show "the system cannof find the file specified" if %tempFolder% doesn't exist
+rmdir /s /q %tempFolder%
+
+rem Make the directory to place the Spotlight pictures
+mkdir %tempFolder%
+
+rem Copy everything from the current folder to %tempFolder%
+rem We use xcopy since Windows doesn't have Linux's cp
+rem "/v" means to verify the copied file. It's a bit costly though
+rem "*" means all files in this folder
+xcopy /v * %tempFolder%
+
+rem Go to the folder where we extracted the Spotlight pictures
+cd %tempFolder%
+
+rem Rename everything to everything.jpg
+ren * *.jpg
+
+rem Start an instance of Explorer at the current window (tempFolder)
+start .
+
+rem Go back to the parent directory
+cd ..
+
+rem We pause execution so that the cmd screen doesn't immediately close
+pause
---
+++ readme.md
@@ -0,0 +1,11 @@
+# View Spotlight pictures
+
+## Information
+A Windows script to view the Spotlight images used in the lockscreen
+
+It saves the pictures in a folder named "tempPictures" inside the Spotlight directory
+
+However, the Spotlight directory is a general directory which contains other non-Spotlight pictures
+
+## Usage
+This script can be ran directly by double-clicking on it or by invoking it through the command prpompt
